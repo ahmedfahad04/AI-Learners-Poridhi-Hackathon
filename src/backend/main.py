4735e66db77cd4504efd8f2b4836dfd1cf85a220
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2025-04-25 14:49:55 +0600

    feat: set up backend with Docker, Prometheus, and FastAPI, including model conversion and requirements

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from routes.routes_products import router as products_router
 from routes.routes_config import router as config_router
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(products_router)
 app.include_router(config_router)
